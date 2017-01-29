9cd43a09a35e335649163e87a52f573342416eed
Nur Rohman <ennurr31@gmail.com>
2017-01-29 11:47:55 +0700

    update

--- app.groovy
+++ app.groovy
@@ -1,22 +1,9 @@
 @RestController
 class ThisWillActuallyRun {
 
-    @Autowired
-    @Qualifier("com.linecorp.channel_secret")
-    String lChannelSecret
-
-    @Autowired
-    @Qualifier("com.linecorp.channel_access_token")
-    String lChannelAccessToken
-
-    @RequestMapping("/callback")
-    public ResponseEntity<String> callback(
-            @RequestHeader("X-Line-Signature") String aXLineSignature,
-            @RequestBody String aPayload)
-    {
-        // compose body
-
-        return new ResponseEntity<String>(HttpStatus.OK)
-    }
+        @RequestMapping("/linebot/callback")
+        String home() {
+            return app.sources(Application.class);
+            }
 
     }
